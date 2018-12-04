4d7fd1d500ae108e96316f06cc0174d51a72019c
Amit <amit.so100@gmail.com>
2018-12-04 10:47:52 +0200

    firebase connection created, creating signup page

--- app/src/main/java/com/example/coursefreak/coursefreak/LoginPage.java
+++ app/src/main/java/com/example/coursefreak/coursefreak/LoginPage.java
@@ -2,12 +2,31 @@ package com.example.coursefreak.coursefreak;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
 
 public class LoginPage extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        this.mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        TextView title = (TextView)findViewById(R.id.titleBox);
+        if(currentUser != null) {
+            title.setText("Welcome ".concat(currentUser.getEmail()));
+        }
     }
 }
