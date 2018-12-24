4d9fc63f427c594e17bd9d318181b2eefa04531c
DxxN96 <denisnastic@campus.technion.ac.il>
2018-12-24 11:48:45 +0200

    Merge branch 'master' of https://github.com/Technion236503/2019a-CourseFreak
    # Conflicts:
    #	app/build.gradle

--- app/src/main/java/com/example/coursefreak/coursefreak/catalog.java
+++ app/src/main/java/com/example/coursefreak/coursefreak/catalog.java
@@ -1,5 +1,6 @@
 package com.example.coursefreak.coursefreak;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,6 +14,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,13 +23,66 @@ import android.view.ViewGroup;
 
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class catalog extends Fragment {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("coursefreak-6c777");
+    final List<Course> res = new ArrayList<>();
+    myRef.child("courses").addValueEventListener(new ValueEventListener() {
+        
+    }
+    Course[] c = FirebaseUtils.allCourses(myRef);
+    public catalog() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.catalog, container, false);
-        return rootView;
+        RecyclerView rv = new RecyclerView(getContext());
+        rv.setLayoutManager(new LinearLayoutManager(getContext()));
+        rv.setAdapter(new SimpleRVAdapter(c));
+        return rv;
+    }
+    public class SimpleRVAdapter extends RecyclerView.Adapter<SimpleViewHolder> {
+        private Course[] dataSource;
+        public SimpleRVAdapter(Course[] dataArgs){
+            dataSource = dataArgs;
+        }
+
+        @Override
+        public SimpleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = new TextView(parent.getContext());
+            SimpleViewHolder viewHolder = new SimpleViewHolder(view);
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(SimpleViewHolder holder, int position) {
+            String txt = dataSource[position].getName() + "-" + dataSource[position].getCourseID();
+            holder.textView.setText(txt);
+        }
+
+        @Override
+        public int getItemCount() {
+            return dataSource.length;
+        }
     }
 
+    /**
+     * A Simple ViewHolder for the RecyclerView
+     */
+    public static class SimpleViewHolder extends RecyclerView.ViewHolder{
+        public TextView textView;
+        public SimpleViewHolder(View itemView) {
+            super(itemView);
+            textView = (TextView) itemView;
+        }
+    }
 }
+
