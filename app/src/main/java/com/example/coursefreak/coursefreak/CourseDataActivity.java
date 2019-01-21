56163881a9b9286530cdfbfc9c31d3073fa19193
Roy <roy.hirsch92@gmail.com>
2019-01-21 20:58:15 +0200

    more changes - roy

--- app/src/main/java/com/example/coursefreak/coursefreak/CourseDataActivity.java
+++ app/src/main/java/com/example/coursefreak/coursefreak/CourseDataActivity.java
@@ -1,18 +1,26 @@
 package com.example.coursefreak.coursefreak;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatCheckBox;
+import android.support.v7.widget.AppCompatSpinner;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -48,12 +56,13 @@ public class CourseDataActivity extends AppCompatActivity {
         setContentView(R.layout.activity_course_data);
         parent_view = findViewById(android.R.id.content);
 
-        initComponent();
-        initCourseData_Intent();
+        initCourseData();
         initToolbar();
+        initButtonListeners();
+        initDropDowns();
     }
 
-    private void initCourseData_Intent(){
+    private void initCourseData(){
         course = new Course("234218","Example Course",2.8,6,8,76.8,"no","hw;pairs");
 
         Intent intent = getIntent();
@@ -185,7 +194,7 @@ public class CourseDataActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    private void initComponent() {
+    private void initDropDowns() {
 
         // text section
         bt_toggle_text = (ImageButton) findViewById(R.id.bt_toggle_text);
@@ -292,5 +301,41 @@ public class CourseDataActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), item.getTitle(), Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initButtonListeners(){
+
+        FloatingActionButton fabPartners = findViewById(R.id.fab_partners);
+
+        fabPartners.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCustomDialog();
+            }
+        });
+    }
+
+
+
+    private void showCustomDialog() {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE); // before
+        dialog.setContentView(R.layout.dialog_partners);
+        dialog.setCancelable(true);
+
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(dialog.getWindow().getAttributes());
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+
+        ((ImageButton) dialog.findViewById(R.id.bt_close)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+        dialog.getWindow().setAttributes(lp);
     }
 }
